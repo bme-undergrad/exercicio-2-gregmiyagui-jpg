5855b4c922d187dadf0c0d16bdb48c80d21943af
Greg Miyagui <greg.miyagui@gmail.com>
2025-09-08 15:59:49 -0300

    commit1

--- exercicio2.m
+++ exercicio2.m
@@ -10,8 +10,21 @@ pkg load optim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
+f = [10 , NG*2]';
+x = [1 , 1]';
 
+A = [0.3 , 0.2;
+     0.2 , 0.25;
+     0.25 , 0.3;
+     1 , 0;
+     0 , 1];
+b = [15 , 10 , 12 , 20 , 50]';
+
+Aeq = [1 , 0;
+       0 , 1];
+beq = [0 , 0]';
+
+[x] = linprog(f , A , b , Aeq , beq)
 
 % mantenha essas duas linhas finais
 blendA = x(1);
